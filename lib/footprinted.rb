71f7f95454b493ee3d080134d290fddb51b758e7
Javi R <4920956+rameerez@users.noreply.github.com>
2024-09-26 17:30:29 +0300

    Initial commit

---
+++ lib/footprinted.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "footprinted/version"
+
+module Footprinted
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/footprinted/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Footprinted
+  VERSION = "0.1.0"
+end
