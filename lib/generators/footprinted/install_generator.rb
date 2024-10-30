2f6de5880c829e677b28ee7cd47e5d1b8c123aed
Javi R <4920956+rameerez@users.noreply.github.com>
2024-10-30 01:22:59 +0000

    Fix initial migration

--- lib/generators/footprinted/install_generator.rb
+++ lib/generators/footprinted/install_generator.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
 require 'rails/generators/base'
+require 'rails/generators/active_record'
 
 module Footprinted
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      include ActiveRecord::Generators::Migration
+
       source_root File.expand_path('templates', __dir__)
 
-      def create_initializer_file
-        template 'footprinted.rb', 'config/initializers/footprinted.rb'
+      def self.next_migration_number(dir)
+        ActiveRecord::Generators::Base.next_migration_number(dir)
       end
 
-      def create_migration
-        generate "migration", "CreateTrackableActivities ip:inet country:string city:string trackable:references{polymorphic} user:references activity_type:string"
+      def create_migration_file
+        migration_template 'create_footprinted_trackable_activities.rb.erb', File.join(db_migrate_path, "create_footprinted_trackable_activities.rb")
       end
+
+      private
+
+      def migration_version
+        "[#{ActiveRecord::VERSION::STRING.to_f}]"
+      end
+
     end
   end
 end
